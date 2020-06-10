43743e888c746af56158078d29db159313cf1bd5
Koichi Shiraishi <zchee.io@gmail.com>
2020-06-11 01:50:54 +0900

    path/filepath/walk: add walk benchmarkpath/filepath/walk

---
+++ path/filepath/walk/benchmark_test.go
@@ -0,0 +1,52 @@
+package walk_benchmark
+
+import (
+	"flag"
+	"go/build"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/karrick/godirwalk"
+
+	"github.com/zchee/go-benchmarks/path/filepath/walk/fastwalk"
+)
+
+var benchDir = flag.String("benchdir", build.Default.GOROOT, "The directory to scan for walk Benchmark")
+
+func BenchmarkFilepathwalk(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		err := filepath.Walk(*benchDir, func(path string, info os.FileInfo, err error) error { return nil })
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkFastWalk(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		err := fastwalk.Walk(*benchDir, func(path string, typ os.FileMode) error { return nil })
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkGodirwalkUnsorted(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		err := godirwalk.Walk(*benchDir, &godirwalk.Options{
+			Callback:      func(_ string, _ *godirwalk.Dirent) error { return nil },
+			ScratchBuffer: make([]byte, os.Getpagesize()),
+			Unsorted:      true,
+		})
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
---
+++ path/filepath/walk/go.mod
@@ -0,0 +1,5 @@
+module github.com/zchee/go-benchmarks/path/filepath/walk
+
+go 1.14
+
+require github.com/karrick/godirwalk v1.15.6
